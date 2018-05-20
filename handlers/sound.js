76d9d09590eb3726f4f75cda3fa56d1fe392e5f6
yamaryoxxxx <yamaryoxxxx@gmail.com>
2018-05-20 13:47:05 +0900

    Update sound.js

--- handlers/sound.js
+++ handlers/sound.js
@@ -1,10 +1,12 @@
 const Sound = require('aplay');
 const player = new Sound();
+player.play(params.filename);
+
 module.exports = function(_params){
     const params = _params;
     return {
         exec: function(){
-            player.play(params.filename);
+            player.resume();
             console.log('play', params.filename);
         }
     }
