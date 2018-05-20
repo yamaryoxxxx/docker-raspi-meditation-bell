2ac8f3fc33bf0f62e50f1eb9890702f89d0701a7
yamaryoxxxx <yamaryoxxxx@gmail.com>
2018-05-20 21:16:04 +0900

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
-git pull
-docker kill meditation-bell
-docker rm meditation-bell
-docker build -t raspi-meditation-bell .
-docker run -d --restart=always --device /dev/snd:/dev/snd --name meditation-bell raspi-meditation-bell
+git pull && \
+docker kill meditation-bell && \
+docker rm meditation-bell && \
+docker build -t yamaryoxxxx/raspi-meditation-bell . && \
+docker push yamaryoxxxx/raspi-meditatioon-bell && \
+docker run -d --restart=always --device /dev/snd:/dev/snd --name meditation-bell yamaryoxxxx/raspi-meditation-bell
