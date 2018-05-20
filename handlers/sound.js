5404e57db2f982d0289580e9b9d22847e4f49ad8
yamaryoxxxx <yamaryoxxxx@gmail.com>
2018-05-20 13:59:05 +0900

    Update sound.js

--- handlers/sound.js
+++ handlers/sound.js
@@ -1,9 +1,13 @@
 const Sound = require('aplay');
-const player = new Sound();
-player.play(params.filename);
+const execSync = require('child_process').execSync;
 
 module.exports = function(_params){
     const params = _params;
+    const player = new Sound();
+    player.play(params.filename);
+    console.log(execSync('amixer scontrols'));
+    console.log(execSync('amixer sset "PCM" 100%'));    
+    
     return {
         exec: function(){
             player.resume();
